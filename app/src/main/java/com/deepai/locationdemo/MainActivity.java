7d11ccd745eb3b8acd7c8ebb5a72a86efbcb659f
HZY <624060373@qq.com>
2016-10-31 14:37:32 +0800

    测试git提交

--- app/src/main/java/com/deepai/locationdemo/MainActivity.java
+++ app/src/main/java/com/deepai/locationdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.deepai.locationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        Log.e("测试git----","提交");
     }
 }
