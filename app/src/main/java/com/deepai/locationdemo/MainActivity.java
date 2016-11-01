e4cb875ca1bd33a0dddf8e5b2d3ad1dc017284c1
root <uana_777@163.com>
2016-11-01 11:43:16 +0800

    添加位置信息

--- app/src/main/java/com/deepai/locationdemo/MainActivity.java
+++ app/src/main/java/com/deepai/locationdemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.deepai.locationdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.deepai.locationdemo.marker.InfoWindowActivity;
+import com.deepai.locationdemo.route.RouteActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
+
+    /**
+     * 位置标注及信息展示
+     */
+    public void onLocationInfo(View view) {
+        InfoWindowActivity.goInto(this);
+    }
+
+    /**
+     * 路径规划
+     */
+    public void onRoutePlan(View view) {
+        RouteActivity.goInto(this);
+    }
+
+    /**
+     * 导航
+     */
+    public void onGuide(View view) {
+
+    }
+
+
 }
