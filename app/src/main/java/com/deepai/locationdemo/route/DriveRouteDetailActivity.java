90775ca9bfef58287267f8eb24c5a96f411f8b90
HZY <624060373@qq.com>
2016-11-01 15:49:20 +0800

    添加导航按钮

--- app/src/main/java/com/deepai/locationdemo/route/DriveRouteDetailActivity.java
+++ app/src/main/java/com/deepai/locationdemo/route/DriveRouteDetailActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,53 +16,63 @@ import com.deepai.locationdemo.util.AMapUtil;
 
 
 public class DriveRouteDetailActivity extends Activity {
-	private DrivePath mDrivePath;
-	private DriveRouteResult mDriveRouteResult;
-	private TextView mTitle, mTitleDriveRoute, mDesDriveRoute;
-	private ListView mDriveSegmentList;
-	private DriveSegmentListAdapter mDriveSegmentListAdapter;
+    private DrivePath mDrivePath;
+    private DriveRouteResult mDriveRouteResult;
+    private TextView mTitle, mTitleDriveRoute, mDesDriveRoute;
+    private ListView mDriveSegmentList;
+    private DriveSegmentListAdapter mDriveSegmentListAdapter;
+    private LinearLayout llGuideTip;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_route_detail);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_detail);
 
-		getIntentData();
-		init();
-	}
+        getIntentData();
+        init();
+    }
 
-	private void init() {
-		mTitle = (TextView) findViewById(R.id.title_center);
-		mTitleDriveRoute = (TextView) findViewById(R.id.firstline);
-		mDesDriveRoute = (TextView) findViewById(R.id.secondline);
-		mTitle.setText("驾车路线详情");
-		String dur = AMapUtil.getFriendlyTime((int) mDrivePath.getDuration());
-		String dis = AMapUtil.getFriendlyLength((int) mDrivePath
-				.getDistance());
-		mTitleDriveRoute.setText(dur + "(" + dis + ")");
-		int taxiCost = (int) mDriveRouteResult.getTaxiCost();
-		mDesDriveRoute.setText("打车约"+taxiCost+"元");
-		mDesDriveRoute.setVisibility(View.VISIBLE);
-		configureListView();
-	}
+    private void init() {
+        mTitle = (TextView) findViewById(R.id.title_center);
+        mTitleDriveRoute = (TextView) findViewById(R.id.firstline);
+        mDesDriveRoute = (TextView) findViewById(R.id.secondline);
+        mTitle.setText("驾车路线详情");
+        String dur = AMapUtil.getFriendlyTime((int) mDrivePath.getDuration());
+        String dis = AMapUtil.getFriendlyLength((int) mDrivePath
+                .getDistance());
+        mTitleDriveRoute.setText(dur + "(" + dis + ")");
+        int taxiCost = (int) mDriveRouteResult.getTaxiCost();
+        mDesDriveRoute.setText("打车约" + taxiCost + "元");
+        mDesDriveRoute.setVisibility(View.VISIBLE);
+        llGuideTip = (LinearLayout) findViewById(R.id.title_guide_tip);
+        llGuideTip.setVisibility(View.VISIBLE);
+        configureListView();
+    }
 
-	private void configureListView() {
-		mDriveSegmentList = (ListView) findViewById(R.id.bus_segment_list);
-		mDriveSegmentListAdapter = new DriveSegmentListAdapter(
-				this.getApplicationContext(), mDrivePath.getSteps());
-		mDriveSegmentList.setAdapter(mDriveSegmentListAdapter);
-	}
+    private void configureListView() {
+        mDriveSegmentList = (ListView) findViewById(R.id.bus_segment_list);
+        mDriveSegmentListAdapter = new DriveSegmentListAdapter(
+                this.getApplicationContext(), mDrivePath.getSteps());
+        mDriveSegmentList.setAdapter(mDriveSegmentListAdapter);
+    }
 
-	private void getIntentData() {
-		Intent intent = getIntent();
-		if (intent == null) {
-			return;
-		}
-		mDrivePath = intent.getParcelableExtra("drive_path");
-		mDriveRouteResult = intent.getParcelableExtra("drive_result");
-	}
+    private void getIntentData() {
+        Intent intent = getIntent();
+        if (intent == null) {
+            return;
+        }
+        mDrivePath = intent.getParcelableExtra("drive_path");
+        mDriveRouteResult = intent.getParcelableExtra("drive_result");
+    }
 
-	public void onBackClick(View view) {
-		this.finish();
-	}
+    /**
+     * 去导航点击
+     */
+    public void onGuideTipClick(View view) {
+
+    }
+
+    public void onBackClick(View view) {
+        this.finish();
+    }
 }
--- app/src/main/java/com/deepai/locationdemo/route/WalkRouteDetailActivity.java
+++ app/src/main/java/com/deepai/locationdemo/route/WalkRouteDetailActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,40 +14,48 @@ import com.deepai.locationdemo.route.adapter.WalkSegmentListAdapter;
 import com.deepai.locationdemo.util.AMapUtil;
 
 public class WalkRouteDetailActivity extends Activity {
-	private WalkPath mWalkPath;
-	private TextView mTitle,mTitleWalkRoute;
-	private ListView mWalkSegmentList;
-	private WalkSegmentListAdapter mWalkSegmentListAdapter;
+    private WalkPath mWalkPath;
+    private TextView mTitle, mTitleWalkRoute;
+    private ListView mWalkSegmentList;
+    private WalkSegmentListAdapter mWalkSegmentListAdapter;
+    private LinearLayout llGuideTip;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_route_detail);
-		getIntentData();
-		mTitle = (TextView) findViewById(R.id.title_center);
-		mTitle.setText("步行路线详情");
-		mTitleWalkRoute = (TextView) findViewById(R.id.firstline);
-		String dur = AMapUtil.getFriendlyTime((int) mWalkPath.getDuration());
-		String dis = AMapUtil
-				.getFriendlyLength((int) mWalkPath.getDistance());
-		mTitleWalkRoute.setText(dur + "(" + dis + ")");
-		mWalkSegmentList = (ListView) findViewById(R.id.bus_segment_list);
-		mWalkSegmentListAdapter = new WalkSegmentListAdapter(
-				this.getApplicationContext(), mWalkPath.getSteps());
-		mWalkSegmentList.setAdapter(mWalkSegmentListAdapter);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_detail);
+        getIntentData();
+        mTitle = (TextView) findViewById(R.id.title_center);
+        mTitle.setText("步行路线详情");
+        mTitleWalkRoute = (TextView) findViewById(R.id.firstline);
+        llGuideTip = (LinearLayout) findViewById(R.id.title_guide_tip);
+        llGuideTip.setVisibility(View.VISIBLE);
+        String dur = AMapUtil.getFriendlyTime((int) mWalkPath.getDuration());
+        String dis = AMapUtil
+                .getFriendlyLength((int) mWalkPath.getDistance());
+        mTitleWalkRoute.setText(dur + "(" + dis + ")");
+        mWalkSegmentList = (ListView) findViewById(R.id.bus_segment_list);
+        mWalkSegmentListAdapter = new WalkSegmentListAdapter(
+                this.getApplicationContext(), mWalkPath.getSteps());
+        mWalkSegmentList.setAdapter(mWalkSegmentListAdapter);
+    }
 
-	}
+    private void getIntentData() {
+        Intent intent = getIntent();
+        if (intent == null) {
+            return;
+        }
+        mWalkPath = intent.getParcelableExtra("walk_path");
+    }
 
-	private void getIntentData() {
-		Intent intent = getIntent();
-		if (intent == null) {
-			return;
-		}
-		mWalkPath = intent.getParcelableExtra("walk_path");
-	}
+    /**
+     * 去导航点击
+     */
+    public void onGuideTipClick(View view) {
 
-	public void onBackClick(View view) {
-		this.finish();
-	}
+    }
 
+    public void onBackClick(View view) {
+        this.finish();
+    }
 }
